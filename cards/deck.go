1dbda4427d57c8f9250d40c861b7184ea835ca99
brainaxe <tanzimrizwan@gmail.com>
2021-10-25 18:31:03 +0600

    added shuffle function

--- cards/deck.go
+++ cards/deck.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -19,7 +21,7 @@ func newDeck() deck {
 
     for _, suit := range cardSuits {
         for _, value := range cardValues {
-            cards = append(cards, suit+ " of " +value)
+            cards = append(cards, value+ " of " +suit)
         }
     }
 
@@ -53,4 +55,14 @@ func newDeckFromFile(filename string) deck {
 
     s := strings.Split(string(bs), ",")
     return deck(s)
+}
+
+func (d deck) shuffle() {
+    source := rand.NewSource(time.Now().UnixNano())
+    r := rand.New(source)
+
+    for i := range d {
+        newPosition := r.Intn(len(d) - 1)
+        d[i], d[newPosition] = d[newPosition], d[i]
+    }
 }
--- cards/main.go
+++ cards/main.go
@@ -3,11 +3,13 @@ package main
 func main() {
     // var card string = "Ace of Spades"
     // card := "Ace of Spades"
-    // cards := newDeck()
+    cards := newDeck()
+    cards.shuffle()
+    cards.print()
     // cards.saveToFile("my_cards")
 
-    cards := newDeckFromFile("my_cards")
-    cards.print()
+    // cards := newDeckFromFile("my_cards")
+    // cards.print()
     
     // hand, remainingCards := deal(cards, 5)
 

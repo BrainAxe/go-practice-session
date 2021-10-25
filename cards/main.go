03ba17ca13132457f39ede93993e766ac98c9972
brainaxe <tanzimrizwan@gmail.com>
2021-10-25 16:39:35 +0600

    write file to hard disk

--- cards/main.go
+++ cards/main.go
@@ -4,9 +4,10 @@ func main() {
     // var card string = "Ace of Spades"
     // card := "Ace of Spades"
     cards := newDeck()
+    cards.saveToFile("my_cards")
     
-    hand, remainingCards := deal(cards, 5)
+    // hand, remainingCards := deal(cards, 5)
 
-    hand.print()
-    remainingCards.print()
+    // hand.print()
+    // remainingCards.print()
 }

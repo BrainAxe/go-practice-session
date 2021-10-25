11ce6e2f37a90487c98986abe0bc1ad97a5870fb
brainaxe <tanzimrizwan@gmail.com>
2021-10-25 16:10:49 +0600

    added deal function

--- cards/deck.go
+++ cards/deck.go
@@ -7,22 +7,26 @@ import "fmt"
 type deck []string
 
 func newDeck() deck {
-		cards := deck{}
+    cards := deck{}
 
-		cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-		cardValues := []string{"Ace", "Two", "Three", "Four"}
+    cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+    cardValues := []string{"Ace", "Two", "Three", "Four"}
 
-		for _, suit := range cardSuits {
-				for _, value := range cardValues {
-					cards = append(cards, suit+ " of " +value)
-				}
-		}
+    for _, suit := range cardSuits {
+        for _, value := range cardValues {
+            cards = append(cards, suit+ " of " +value)
+        }
+    }
 
-		return cards
+    return cards
 }
 
 func (d deck) print() {
-		for i, card := range d {
-				fmt.Println(i, card)
-		}
+    for i, card := range d {
+        fmt.Println(i, card)
+    }
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+    return d[:handSize], d[handSize:]
 }
--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,12 @@
 package main
 
 func main() {
-		// var card string = "Ace of Spades"
-		// card := "Ace of Spades"
-		cards := newDeck()
-		cards.print()
+    // var card string = "Ace of Spades"
+    // card := "Ace of Spades"
+    cards := newDeck()
+    
+    hand, remainingCards := deal(cards, 5)
 
+    hand.print()
+    remainingCards.print()
 }

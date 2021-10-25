d54c47097918be402c62487f1303d3e0d9784f4b
brainaxe <tanzimrizwan@gmail.com>
2021-10-25 14:50:02 +0600

    slice and for loop

--- cards/main.go
+++ cards/main.go
@@ -4,8 +4,15 @@ import "fmt"
 
 func main() {
 		// var card string = "Ace of Spades"
-		card := newCard()
-		fmt.Println(card)	
+		// card := newCard()
+		// fmt.Println(card)	
+		cards := []string{"Ace of Hearts", newCard()} // Slice
+		cards = append(cards, "Six of Spades")
+
+		for i, card := range cards {
+			fmt.Println(i, card)
+		}
+
 }
 
 func newCard() string {

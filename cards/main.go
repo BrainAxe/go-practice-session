dbded8e7082e73e2ecfeb801f047aa249be85c5e
brainaxe <tanzimrizwan@gmail.com>
2021-10-25 15:48:53 +0600

    added new deck function

--- cards/main.go
+++ cards/main.go
@@ -2,15 +2,8 @@ package main
 
 func main() {
 		// var card string = "Ace of Spades"
-		// card := newCard()
-		// fmt.Println(card)	
-		cards := deck{"Ace of Hearts", newCard()} // Slice
-		cards = append(cards, "Six of Spades")
-
+		// card := "Ace of Spades"
+		cards := newDeck()
 		cards.print()
 
-}
-
-func newCard() string {
-		return "Five of Diamonds"
 }

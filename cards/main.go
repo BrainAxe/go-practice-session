a76a3d115cf7da7d20077303989d764108f65077
brainaxe <tanzimrizwan@gmail.com>
2021-10-25 15:37:22 +0600

    added deck type and receiver function

--- cards/main.go
+++ cards/main.go
@@ -1,17 +1,13 @@
 package main
 
-import "fmt"
-
 func main() {
 		// var card string = "Ace of Spades"
 		// card := newCard()
 		// fmt.Println(card)	
-		cards := []string{"Ace of Hearts", newCard()} // Slice
+		cards := deck{"Ace of Hearts", newCard()} // Slice
 		cards = append(cards, "Six of Spades")
 
-		for i, card := range cards {
-			fmt.Println(i, card)
-		}
+		cards.print()
 
 }
 
